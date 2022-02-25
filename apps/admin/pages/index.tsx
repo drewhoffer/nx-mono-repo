464abfb579540ab934cf576cbb35b5364d5c7660
Fresh <drewsdhoffer@gmail.com>
2022-02-25 17:17:11 -0500

    test 5

--- apps/admin/pages/index.tsx
+++ apps/admin/pages/index.tsx
@@ -6,7 +6,7 @@ export function Index() {
    *
    * Note: The corresponding styles are in the ./index.css file.
    */
-  return <div className={styles.page}>Rerun action #2</div>;
+  return <div className={styles.page}>Rerun action #4</div>;
 }
 
 export default Index;
