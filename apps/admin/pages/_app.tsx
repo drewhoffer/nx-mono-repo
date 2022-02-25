79d97da8c5c1e0f7a4b9c38d00ae6bd0f3870e1a
Fresh <drewsdhoffer@gmail.com>
2022-02-25 16:58:55 -0500

    test #3

--- apps/admin/pages/_app.tsx
+++ apps/admin/pages/_app.tsx
@@ -6,7 +6,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to admin!</title>
+        <title>Welcome to admin!!</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
